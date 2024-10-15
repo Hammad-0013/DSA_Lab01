1bc8d0b0172916390b02c41d3f45e80123f2f87b
Hammad-0013 <l1s23bscs0013@ucp.edu.pk>
2024-10-15 12:28:26 +0500

    Add files via upload
    Sorting of an Array in Ascending Order

---
+++ Source.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int size = 5;
+	int arr[5] = { 2, 8, 4, 5, 1 };
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i+1; j < size; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				int tem = arr[i];
+				arr[i] = arr[j];
+				arr[j] = tem;
+			}
+		}
+	}
+	cout << "Ascending Order Array is: ";
+	for (int i = 0; i < size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+	system("pause");
+	return 0;
+}
