229a5e1c0758fed52f5a8a507f95ab099119b53d
Hammad-0013 <l1s23bscs0013@ucp.edu.pk>
2024-10-15 12:34:19 +0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -4,9 +4,18 @@ int main()
 {
 	int size = 5;
 	int arr[5] = { 2, 8, 4, 5, 1 };
+
+	cout << "Array of 5 elements is: ";
+	for (int i = 0; i<size; i++)
+	{
+		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+
 	for (int i = 0; i < size; i++)
 	{
-		for (int j = i+1; j < size; j++)
+		for (int j = i + 1; j < size; j++)
 		{
 			if (arr[i] > arr[j])
 			{
